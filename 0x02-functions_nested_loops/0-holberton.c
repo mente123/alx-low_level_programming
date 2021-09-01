71adc783d2a8c7bffe9fe2723bb43ec2fb4312cb
Mentesenot Kibebew <mkebebew28@gmail.com>
2021-09-01 20:52:25 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,18 +4,15 @@
  * Description: prints holberton on new line
  * Return: 0
  */
-
 int main(void)
 {
 	char p[8] = "_putchar";
 	int i = 0;
-
 	while (i <= 7)
 	{
 	_putchar(p[i]);
 	++i;
 	}
 	_putchar('\n');
-
 	return (0);
 }
