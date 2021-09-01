a3e64340d58fd1782562cfc6c99ced1abf74bae5
Mentesenot Kibebew <mkebebew28@gmail.com>
2021-09-01 21:02:54 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,16 +7,16 @@
 
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	while (i < 10)
-	{
-	char a = 'a';
-	while (a <= 'z')
-	{
-	_putchar(a);
-	++a;
-	}
-	++i;
-	_putchar('\n');
-	}
+int i = 0;
+while (i < 10)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+++a;
+}
+++i;
+_putchar('\n');
+}
 }
