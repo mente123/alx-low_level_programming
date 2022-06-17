909b3dcded76d81028bb521a3e238062a2152524
Mentesenot Kibebew <mkebebew28@gmail.com>
2022-06-17 11:33:33 -0700

    Add files via upload

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - concatenates two strings.
+ * @dest: Destination string
+ * @src: String to be added
+ * @n: number of bytes to add (or array elements)
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i]; i++)
+	;
+
+	j = 0;
+	while (n > j && src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+
+	return (dest);
+}
